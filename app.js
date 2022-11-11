4f683a6945c03df5ea03b2703458ef8d0214b757
Franco Lorca <lorcafrancog@gmail.com>
2022-11-11 17:56:16 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+function moveToRandomPosition(element) {
+  element.style.position = "absolute";
+  element.style.top =
+    Math.random() * (window.innerHeight - element.offsetHeight) + "px";
+  element.style.left =
+    Math.random() * (window.innerWidth - element.offsetWidth) + "px";
+}
+
+let btnYes = document.getElementById("btn_yes");
+let btnNo = document.getElementById("btn_no");
+let sectionSexMode = document.getElementsByClassName("sex_mode")[0];
+
+btnNo.addEventListener("mouseenter", function (e) {
+  moveToRandomPosition(e.target);
+});
+
+btnYes.addEventListener("click", function (e) {
+  alert("I know that you will tell YES. Married now and will have children. I LOVE YOU!!! ❤️")
+  sectionSexMode.style.display = "block"
+  const music = new Audio('./img/img_modo_hot.mp3')
+  music.play()
+});
